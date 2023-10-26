40745b107519e7fdc621f119943edf9948875b97
Ghaleb Hamadi <ghaleb.hamadi@outlook.com>
2023-10-27 00:19:32 +0300

    cleanup tests

--- tests/unit/utils/string.test.ts
+++ tests/unit/utils/string.test.ts
@@ -6,7 +6,6 @@ describe('stripInterleavingPairs Function', () => {
       expect(stripInterleavingPairs('')).toBe('');
       expect(stripInterleavingPairs('abc')).toBe('abc');
       expect(stripInterleavingPairs('ab1c')).toBe('ab1c');
-      expect(stripInterleavingPairs('ab1c')).toBe('ab1c');
       expect(stripInterleavingPairs('ab1cd2')).toBe('ab1cd2');
       expect(stripInterleavingPairs('1ab23c')).toBe('1ab23c');
     });
@@ -16,54 +15,54 @@ describe('stripInterleavingPairs Function', () => {
       expect(stripInterleavingPairs('1a2')).toBe('1a2');
     });
 
-    test('Captures multiple interleaving pairs', () => {
-      expect(stripInterleavingPairs('a1b2#c3d4e5')).toBe('a#c');
-      expect(stripInterleavingPairs('1a2b#3c4d5e')).toBe('1#3');
-    });
-
-    test('Reduces the length of the string containing interleaving pairs', () => {
+    test('Reduces length of any string containing interleaving pairs', () => {
       expect(stripInterleavingPairs('a1b2').length).toBeLessThan(4);
       expect(stripInterleavingPairs('1a2b').length).toBeLessThan(4);
-      expect(stripInterleavingPairs('a1c2').length).toBeLessThan(4);
-      expect(stripInterleavingPairs('1a2c').length).toBeLessThan(4);
-      expect(stripInterleavingPairs('a1A2').length).toBeLessThan(4);
-      expect(stripInterleavingPairs('1a2A').length).toBeLessThan(4);
-      expect(stripInterleavingPairs('a1B2').length).toBeLessThan(4);
-      expect(stripInterleavingPairs('1a2B').length).toBeLessThan(4);
+    });
+
+    test('Checking letter positions & sequence is case insensitive', () => {
+      expect(stripInterleavingPairs('a1B2').length).toBe(1);
+      expect(stripInterleavingPairs('1A2b').length).toBe(1);
+    });
+
+    test('Captures all interleaving pairs', () => {
+      expect(stripInterleavingPairs('a1b2#c3d4e5')).toBe('a#c');
+      expect(stripInterleavingPairs('1a2b#3c4d5e')).toBe('1#3');
     });
   });
 
   describe('Length reduction strategy', () => {
-    test('Reduces the length by 25% in the best case scenario', () => {
+    test('Reduces length by 25% in the best case scenario', () => {
       expect(stripInterleavingPairs('a1C6').length).toBe(3);
       expect(stripInterleavingPairs('1a6C').length).toBe(3);
     });
 
-    test('Reduces the length by 50% when letters are of the same case', () => {
+    test('Reduces length by 50% when letters are of the same case', () => {
       expect(stripInterleavingPairs('a3c6').length).toBe(2);
       expect(stripInterleavingPairs('3a6c').length).toBe(2);
     });
 
-    test('Reduces the length by 50% when numbers match the position of the letters', () => {
+    test('Reduces length by 50% when numbers match the position of the letters', () => {
       expect(stripInterleavingPairs('a1C3').length).toBe(2);
       expect(stripInterleavingPairs('1a3C').length).toBe(2);
     });
 
-    test('Reduces the length by 50% when characters are sequential', () => {
+    test('Reduces length by 50% when characters are sequential', () => {
       expect(stripInterleavingPairs('a3B3').length).toBe(2);
       expect(stripInterleavingPairs('3a3B').length).toBe(2);
     });
 
-    test('Reduces the length by 75% when multiple criteria are met', () => {
-      // Same case + Sequential letters
+    test('Reduces length by 75% when same case & sequential characters are found', () => {
       expect(stripInterleavingPairs('a2b3c5d6').length).toBe(2);
       expect(stripInterleavingPairs('2a3b5c6d').length).toBe(2);
+    });
 
-      // Same case + Numbers match letter position
+    test('Reduces length by 75% when same case & matching number-letter positions are found', () => {
       expect(stripInterleavingPairs('a1c3e5g7').length).toBe(2);
       expect(stripInterleavingPairs('1a3c5e7g').length).toBe(2);
+    });
 
-      // Sequential letters + Numbers match letter position
+    test('Reduces length by 75% when sequential letters & matching number-letter positions are found', () => {
       expect(stripInterleavingPairs('A1b2').length).toBe(1);
       expect(stripInterleavingPairs('1A2B').length).toBe(1);
     });
