b7a8c56dec0ac18c42c317047a319ad70b0580b0
Ghaleb Hamadi <ghaleb.hamadi@outlook.com>
2023-11-04 18:14:17 +0200

    add tests for permute

--- src/lib/utils/array.ts
+++ src/lib/utils/array.ts
@@ -3,17 +3,20 @@ export function permute<T>(arr: T[]) {
     if (input.length === 0) {
       permutations.push(output.slice());
     }
-
+    let prev: T | null = null;
     for (let i = 0; i < input.length; i++) {
+      if (prev === input[i]) continue;
+
       let newInput = input.slice(0, i).concat(input.slice(i + 1));
 
       output.push(input[i]);
       generate(newInput, output, permutations);
       output.pop();
+      prev = input[i];
     }
 
     return permutations;
   }
 
-  return generate(arr, [], []);
+  return generate(arr.sort(), [], []);
 }
